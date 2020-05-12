dfedbbfbae90e21e6bea6b06cc5aecf263beb980
TimotheRuffino <timothe.ruffino@gmail.com>
2020-05-12 16:47:50 +0200

    commit

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'herokuapp.com',
+  :domain => 'fathomless-shelf-69499.herokuapp.com/',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
