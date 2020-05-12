662a6103208313b3804e0ac64a4365b99a628e38
TimotheRuffino <timothe.ruffino@gmail.com>
2020-05-12 20:43:58 +0200

    add image

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,45 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-yopmails = ["he@yopmail.com", "testdefail@yopmail.com"]
-cities = ["Paris", "Marseille", "Lyon", "La Vallette", "Rome", "Deauville"]
 
-User.destroy_all
-Event.destroy_all
-Attendance.destroy_all
 
-users = []
-events = []
+#User.destroy_all
+#Event.destroy_all
+#Attendance.destroy_all
 
-10.times do |i|
-    users << User.create!(
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        email: "testdefail@yopmail.com",
-        description: Faker::Lorem.paragraphs,
-        password: "testos"
+#users = []
+#events = []
+
+#10.times do |i|
+ #   users << User.create!(
+  #      first_name: Faker::Name.first_name,
+   #     last_name: Faker::Name.last_name,
+    #    email: "testdefail@yopmail.com",
+     #   description: Faker::Lorem.paragraphs,
+     #   password: "testos"
         
-    )
+   # )
     
-end
+#end
 
-10.times do |i|
-    events << Event.create!(
-        start_date: Time.parse("2020-08-12"),
-        duration: 5 * rand(1..100),
-        title: Faker::TvShows::Simpsons.location,
-        description: Faker::Lorem.paragraphs,
-        price: rand(1..999),
-        location: cities.sample,
-        admin: users.sample
-    )
+#10.times do |i|
+ #   events << Event.create!(
+  #      start_date: Time.parse("2020-08-12"),
+   #     duration: 5 * rand(1..100),
+    #    title: Faker::TvShows::Simpsons.location,
+    #    description: Faker::Lorem.paragraphs,
+    #    price: rand(1..999),
+     #   location: cities.sample,
+     #   admin: users.sample
+    #)
     
-end
+#end
 
-10.times do |i|
-    attendance = Attendance.create!(
-        user: users.sample,
-        event: events.sample
-    )
+#10.times do |i|
+ #   attendance = Attendance.create!(
+  #      user: users.sample,
+   #     event: events.sample
+ #   )
 
 end
