2367963f3d758256bdc5e56757d268303a1405a5
TimotheRuffino <timothe.ruffino@gmail.com>
2020-05-13 16:49:28 +0200

    ajout création event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,8 +3,38 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def new
+    @events = Event.new
+  end
+
   def show
     # Méthode qui récupère le potin concerné et l'envoie à la view show (show.html.erb) pour affichage
     @events = Event.find(params[:id])
   end
+
+
+  def create
+    @events = Event.new(event_params)
+    puts @events.title
+    if @events.save
+      flash[:success] = "Your event has been created."
+      redirect_to event_path(@events.id)
+    else
+      messages = []
+      if @events.errors.any? 
+        @events.errors.full_messages.each do |message| 
+          messages << message
+        end 
+        flash[:error] = "You failed, find the following errors :#{messages.join(" ")}"
+        render 'new'
+      end
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:events).permit(:title, :location, :duration, :description, :price, :start_date, :admin_id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,34 @@
 class UsersController < ApplicationController
   
+  before_action :authenticate_user!, only: [:show, :edit, :update]
+  #before_action :own_profil
+
   def index
     @users = User.all
   end
+
+  #def authenticate_user
+   # unless current_user
+      
+    #  redirect_to new_user_session_path
+    #end
+  #end
+
+  #def own_profil
+   # if current_user.id == params[:id]
+     
+    #  redirect_to new_user_session_path
+   # end
+  #end
+
   
   def show
-    @users = User.find(params[:id])
     @events_user = User.find(params[:id]).events
-  end
+    @users = User.find(params[:id])
+        if current_user.id != @users.id
+            flash[:error] = "You are not the allowed."
+          redirect_to root_path
+        end
+end
+
 end
